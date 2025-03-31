81728a84cecbfa2463c37b00fcc91e48e973172f
k930503 <k930503@gmail.com>
2025-03-31 14:34:24 +0900

    Update with new preconfirmation changes

--- eth/sbbclient/sbb_client.go
+++ eth/sbbclient/sbb_client.go
@@ -137,28 +137,37 @@ func (s *SbbClient) requestToSbb() {
 
 			log.Debug("Successfully updated tx_orderer info")
 
-			err = s.finalizeBlock(s.sbbCtx, requestPlatformBlockNumber, txOrdererRpcUrls, leaderTxOrdererIndex, validTxOrdererAddresses)
+			//err = s.finalizeBlock(s.sbbCtx, requestPlatformBlockNumber, txOrdererRpcUrls, leaderTxOrdererIndex, validTxOrdererAddresses)
+			//if err != nil {
+			//	log.Errorf("failed to finalize block, error: %v", err)
+			//	timer.Reset(100 * time.Millisecond)
+			//	break
+			//}
+			//
+			//time.Sleep(300 * time.Millisecond)
+
+			//if err = RetryWithCount(s.sbbCtx, func() error {
+			//	transactions, err := s.getRawTransactions(s.sbbCtx, txOrdererRpcUrls, leaderTxOrdererIndex)
+			//	if err != nil {
+			//		log.Errorf("failed to get raw transactions, error: %v", err)
+			//		return err
+			//	}
+			//	s.blockTransactionsCh <- &BlockTransactions{blockNumber: s.finalizedBlockNumber, transactions: transactions}
+			//	return nil
+			//}, 100*time.Millisecond, 600); err != nil {
+			//	log.Errorf("getRawTransactions error: %v", err)
+			//	timer.Reset(100 * time.Millisecond)
+			//	break
+			//}
+
+			transactions, err := s.getRawTransactions(s.sbbCtx, txOrdererRpcUrls, leaderTxOrdererIndex, validTxOrdererAddresses, requestPlatformBlockNumber)
 			if err != nil {
-				log.Errorf("failed to finalize block, error: %v", err)
+				log.Errorf("failed to get raw transactions, error: %v", err)
 				timer.Reset(100 * time.Millisecond)
 				break
 			}
 
-			time.Sleep(300 * time.Millisecond)
-
-			if err = RetryWithCount(s.sbbCtx, func() error {
-				transactions, err := s.getRawTransactions(s.sbbCtx, txOrdererRpcUrls, leaderTxOrdererIndex)
-				if err != nil {
-					log.Errorf("failed to get raw transactions, error: %v", err)
-					return err
-				}
-				s.blockTransactionsCh <- &BlockTransactions{blockNumber: s.finalizedBlockNumber, transactions: transactions}
-				return nil
-			}, 100*time.Millisecond, 600); err != nil {
-				log.Errorf("getRawTransactions error: %v", err)
-				timer.Reset(100 * time.Millisecond)
-				break
-			}
+			s.blockTransactionsCh <- &BlockTransactions{blockNumber: s.finalizedBlockNumber, transactions: transactions}
 
 			endTime := time.Now().UnixMilli()
 			duration := endTime - startTime
@@ -394,24 +403,51 @@ func (s *SbbClient) finalizeBlock(ctx context.Context, platformBlockNumber uint6
 	return errors.New("no tx_orderer")
 }
 
-func (s *SbbClient) getRawTransactions(ctx context.Context, txOrdererRpcUrls []string, leaderTxOrdererIndex *uint64) ([][]byte, error) {
+func (s *SbbClient) getRawTransactions(ctx context.Context, txOrdererRpcUrls []string, leaderTxOrdererIndex *uint64, txOrdererAddresses []string, platformBlockHeight uint64) ([][]byte, error) {
 	reqCtx, reqCancel := context.WithTimeout(ctx, 10*time.Second)
 	defer reqCancel()
-
-	params := GetRawTransactionsParams{
-		RollupId:          s.blockchainService.Config().RollupId,
-		RollupBlockHeight: s.finalizedBlockNumber,
-	}
-
-	body := newJsonRpcRequest(GetRawTransactionList, params)
 
 	res := &GetRawTransactionsResponse{}
 	txOrdererCount := len(txOrdererRpcUrls)
 
 	for i := 0; i < txOrdererCount; i++ {
+		nextTxOrdererIndex, err := s.getNextLeaderTxOrdererIndex(uint64(txOrdererCount), *leaderTxOrdererIndex)
+		if err != nil {
+			return nil, err
+		}
+
+		leaderChangeMessage := LeaderChangeMessage{
+			RollupId:                      s.blockchainService.Config().RollupId,
+			ExecutorAddress:               "0xE34aaF64b29273B7D567FCFc40544c014EEe9970",
+			CurrentLeaderTxOrdererAddress: strings.ToLower(txOrdererAddresses[*leaderTxOrdererIndex]),
+			NextLeaderTxOrdererAddress:    strings.ToLower(txOrdererAddresses[*nextTxOrdererIndex]),
+			PlatformBlockHeight:           platformBlockHeight,
+		}
+
+		messageBytes, err := json.Marshal(leaderChangeMessage)
+		if err != nil {
+			log.Error("Error converting message to bytes: %v", err)
+			return nil, err
+		}
+
+		h := keccak256.Hash(messageBytes)
+
+		signature, err := crypto.Sign(h, s.sequencerPrivateKey)
+		if err != nil {
+			log.Error("Error signing message: %v", err)
+			return nil, err
+		}
+
+		params := GetRawTransactionsParams{
+			LeaderChangeMessage: leaderChangeMessage,
+			RollupSignature:     "0x" + Bytes2Hex(signature),
+		}
+
+		body := newJsonRpcRequest(GetRawTransactionList, params)
+
 		if err := s.httpClient.Send(reqCtx, txOrdererRpcUrls[*leaderTxOrdererIndex], body, res); err != nil {
 			if !strings.Contains(err.Error(), "connection refused") {
-				return nil, fmt.Errorf("failed to send get_raw_transaction_list request to SBB: %s height %d url %s now %d", err.Error(), params.RollupBlockHeight, txOrdererRpcUrls[*leaderTxOrdererIndex], time.Now().UnixMilli())
+				return nil, fmt.Errorf("failed to send get_raw_transaction_list request to SBB: %s url %s now %d", err.Error(), txOrdererRpcUrls[*leaderTxOrdererIndex], time.Now().UnixMilli())
 			}
 
 			log.Warn("failed to get raw transactions due to no tx_orderer found. retrying with a different tx_orderer")
