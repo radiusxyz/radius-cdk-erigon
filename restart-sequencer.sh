4d6fcdf2a85116de952015e97b00ad1e30a44319
k930503 <k930503@gmail.com>
2025-05-15 22:02:49 +0900

    chore

--- restart-sequencer.sh
+++ restart-sequencer.sh
@@ -1,10 +1,10 @@
 make cdk-erigon
 
-docker cp ./build/bin/cdk-erigon cdk-erigon-sequencer-001:/usr/local/bin/cdk-erigon
+docker cp ./build/bin/cdk-erigon cdk-erigon-sequencer-001--2fa28f045b174a42b471b95b9ba6c42e:/usr/local/bin/cdk-erigon
 
-docker exec -it cdk-erigon-sequencer-001 chmod +x /usr/local/bin/cdk-erigon
+docker exec -it cdk-erigon-sequencer-001--2fa28f045b174a42b471b95b9ba6c42e chmod +x /usr/local/bin/cdk-erigon
 
-docker exec -it cdk-erigon-sequencer-001 pkill cdk-erigon
+docker exec -it cdk-erigon-sequencer-001--2fa28f045b174a42b471b95b9ba6c42e pkill cdk-erigon
 
-docker exec -it cdk-erigon-sequencer-001 \
+docker exec -it cdk-erigon-sequencer-001--2fa28f045b174a42b471b95b9ba6c42e \
   /usr/local/share/proc-runner/proc-runner.sh cdk-erigon --config /etc/cdk-erigon/config.yaml
