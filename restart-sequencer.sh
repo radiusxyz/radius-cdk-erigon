86e5dd62b747cf61f22d01d2c65660f6f490dd0b
k930503 <k930503@gmail.com>
2025-05-16 10:58:24 +0900

    chore

--- restart-sequencer.sh
+++ restart-sequencer.sh
@@ -9,11 +9,13 @@ docker cp ./build/bin/cdk-erigon cdk-erigon-sequencer-001--ac865401432d44f996b3b
 docker exec -it cdk-erigon-sequencer-001--ac865401432d44f996b3b05151788ba3 chmod +x /usr/local/bin/cdk-erigon \
   && echo "[✔] Granted executable permission to binary"
 
-## 4. Kill existing process
-#docker exec -it cdk-erigon-sequencer-001--ac865401432d44f996b3b05151788ba3 pkill cdk-erigon \
-#  && echo "[✔] Terminated existing cdk-erigon process"
-#
-## 5. Start new process
-#docker exec -it cdk-erigon-sequencer-001--ac865401432d44f996b3b05151788ba3 \
-#  /usr/local/share/proc-runner/proc-runner.sh /usr/local/bin/cdk-erigon --config /etc/cdk-erigon/config.yaml \
-#  && echo "[✔] Successfully started new cdk-erigon process"
+sleep 5
+
+# 4. Kill existing process
+docker exec -it cdk-erigon-sequencer-001--ac865401432d44f996b3b05151788ba3 pkill cdk-erigon \
+  && echo "[✔] Terminated existing cdk-erigon process"
+
+# 5. Start new process
+docker exec -it cdk-erigon-sequencer-001--ac865401432d44f996b3b05151788ba3 \
+  /usr/local/share/proc-runner/proc-runner.sh /usr/local/bin/cdk-erigon --config /etc/cdk-erigon/config.yaml \
+  && echo "[✔] Successfully started new cdk-erigon process"
