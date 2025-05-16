df0a5b87e9bdb4b504b1607389b25f82fb7bb834
k930503 <k930503@gmail.com>
2025-05-16 11:35:15 +0900

    edit: update restart-sequencer.sh

--- restart-sequencer.sh
+++ restart-sequencer.sh
@@ -1,25 +1,15 @@
-## 1. Build
-#make cdk-erigon && echo "[✔] Successfully built cdk-erigon binary"
-#
-## 2. Copy binary into container
-#docker cp ./build/bin/cdk-erigon cdk-erigon-sequencer-001--a7e3437e78364c7ab2a4e5f96628ecc4:/usr/local/bin/cdk-erigon \
-#  && echo "[✔] Successfully copied binary into container"
-#
-## 3. Grant execution permission
-#docker exec -it cdk-erigon-sequencer-001--a7e3437e78364c7ab2a4e5f96628ecc4 chmod +x /usr/local/bin/cdk-erigon \
-#  && echo "[✔] Granted executable permission to binary"
-#
-## 4. Kill existing process
-#docker exec -it cdk-erigon-sequencer-001--a7e3437e78364c7ab2a4e5f96628ecc4 pkill cdk-erigon \
-#  && echo "[✔] Terminated existing cdk-erigon process"
-#
-#sleep 5
+make cdk-erigon && echo "[✔] Successfully built cdk-erigon binary"
 
-# 6. Restart container (since it died)
-docker start cdk-erigon-sequencer-001--a7e3437e78364c7ab2a4e5f96628ecc4 \
-  && echo "[✔] Container restarted"
+kurtosis file copy ./build/bin/cdk-erigon cdk cdk-erigon-sequencer-001:/usr/local/bin/cdk-erigon \
+  && echo "[✔] Successfully copied binary into container"
 
-# 5. Start new process
-docker exec -it cdk-erigon-sequencer-001--a7e3437e78364c7ab2a4e5f96628ecc4 \
-  /usr/local/share/proc-runner/proc-runner.sh cdk-erigon --config /etc/cdk-erigon/config.yaml \
+kurtosis service exec cdk cdk-erigon-sequencer-001 "chmod +x /usr/local/bin/cdk-erigon" \
+  && echo "[✔] Granted executable permission to binary"
+
+kurtosis service exec cdk cdk-erigon-sequencer-001 "pkill -SIGTRAP \"proc-runner.sh\"" || true \
+  && echo "[✔] Terminated existing cdk-erigon process"
+
+sleep 1
+
+kurtosis service exec cdk cdk-erigon-sequencer-001 "pkill -SIGINT \"cdk-erigon\"" || true \
   && echo "[✔] Successfully started new cdk-erigon process"
