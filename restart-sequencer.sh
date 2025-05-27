d4412ec4e19fad03e08a0daff7a80330acf4ac06
k930503 <k930503@gmail.com>
2025-05-27 18:28:17 +0900

    add: restart-sequencer.sh

--- restart-sequencer.sh
+++ restart-sequencer.sh
@@ -1,15 +1,17 @@
+set -e  # 하나라도 실패하면 즉시 종료
+
 make cdk-erigon && echo "[✔] Successfully built cdk-erigon binary"
 
-kurtosis files copy ./build/bin/cdk-erigon cdk cdk-erigon-sequencer-001:/usr/local/bin/cdk-erigon \
-  && echo "[✔] Successfully copied binary into container"
+kurtosis files copy ./build/bin/cdk-erigon cdk cdk-erigon-sequencer-001:/usr/local/bin/cdk-erigon
+echo "[✔] Successfully copied binary into container"
 
-kurtosis service exec cdk cdk-erigon-sequencer-001 "chmod +x /usr/local/bin/cdk-erigon" \
-  && echo "[✔] Granted executable permission to binary"
+kurtosis service exec cdk cdk-erigon-sequencer-001 "chmod +x /usr/local/bin/cdk-erigon"
+echo "[✔] Granted executable permission to binary"
 
-kurtosis service exec cdk cdk-erigon-sequencer-001 "pkill -SIGTRAP \"proc-runner.sh\"" \
-  && echo "[✔] Terminated existing cdk-erigon process"
+kurtosis service exec cdk cdk-erigon-sequencer-001 "pkill -SIGTRAP \"proc-runner.sh\""
+echo "[✔] Terminated existing cdk-erigon process"
 
 sleep 1
 
-kurtosis service exec cdk cdk-erigon-sequencer-001 "pkill -SIGINT \"cdk-erigon\"" \
-  && echo "[✔] Successfully started new cdk-erigon process"
+kurtosis service exec cdk cdk-erigon-sequencer-001 "pkill -SIGINT \"cdk-erigon\""
+echo "[✔] Successfully started new cdk-erigon process"
